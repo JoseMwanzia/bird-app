26b87cfccbe4c825a43a2d0377ebe6f807c2dfa0
joseph mwanzia <makamwanzia@gmail.com>
2023-08-03 19:30:50 +0300

    Corrected burd_test.rb quotes

--- test/models/bird_test.rb
+++ test/models/bird_test.rb
@@ -5,12 +5,12 @@ require 'test_helper'
 class BirdTest < ActiveSupport::TestCase
   test 'should not save bird without name' do
     bird = Bird.new
-    assert_not bird.save, "saved a bird without a name"
+    assert_not bird.save, 'saved a bird without a name'
     # assert true
   end
 
   test 'should save bird with name' do
-    bird = Bird.new(name: "Test")
+    bird = Bird.new(name: 'Test')
     assert bird.save, 'idk test' do
     end
   end
